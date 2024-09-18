b9a028b000ad9944bd2a1dbd05bd2b87d8e0df41
clembnl <cl.bonal@free.fr>
2024-09-18 13:05:50 +0200

    v1.14 Portfolio freelance France

---
+++ assets/ga4.c3afe914.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.3d936219.js";import{i}from"./vue-gtag.esm.0465f79b.js";var s=a(({app:o})=>{o.use(i,{config:{id:"G-S2RPNQWLWX"}})});export{s as default};
--- assets/ga4.ca87f4d9.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index.9903d5cb.js";import{i}from"./vue-gtag.esm.68ef391d.js";var s=a(({app:o})=>{o.use(i,{config:{id:"G-S2RPNQWLWX"}})});export{s as default};
